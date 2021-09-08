ca5f18ad8996d99f7732bcdae339564af241b6a6
alessandroalessio <info@a2area.it>
2021-09-08 13:35:18 +0200

    Setup base for upload file Excel

--- resources/views/admin/import-year-stats-data.blade.php
+++ resources/views/admin/import-year-stats-data.blade.php
@@ -10,6 +10,29 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <p>Upload fantacalcio.it stats file for Years</p>
+                    <form action="#">
+                        <div class="grid grid-flow-col auto-cols-max md:auto-cols-min">
+                            <div><label>{{ __('Year') }}</label></div>
+                            <div>
+                                <select name="year">
+                                    <?php for ( $y=2015; $y<=2035; $y++ ) :
+                                        $y_end_season = substr($y+1, 2, 2);  ?>
+                                        <option value=""><?php echo $y.'/'.$y_end_season ?></option>
+                                    <?php endfor; ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="grid grid-flow-col auto-cols-max md:auto-cols-min">
+                            <div><label>File Excel</label></div>
+                            <div>
+                                <input type="file" name="excel_file">
+                            </div>
+                        </div>
+                        <div class="grid grid-flow-col auto-cols-max md:auto-cols-min">
+                            <div><input type="submit" value="Upload"></div>
+                        </div>
+
+                    </form>
                 </div>
             </div>
         </div>
